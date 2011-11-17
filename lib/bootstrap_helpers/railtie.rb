f317ca17a42ed274235732737b52f1db7006ddfd
Sergey Pchelincev <jalkoby91@gmail.com>
2011-11-17 18:29:18 +0200

    Fix inclusion issue

--- lib/bootstrap_helpers/railtie.rb
+++ lib/bootstrap_helpers/railtie.rb
@@ -3,7 +3,7 @@ require 'bootstrap_helpers/view_helpers'
 module BootstrapHelpers
   class Railtie < Rails::Railtie
     initializer 'bootstrap_helpers.view_helpers' do
-      ActionView::Base.include, ViewHelpers
+      ActionView::Base.include ViewHelpers
     end
   end
 end
