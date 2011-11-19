53e7fd5545267080598931d63e147bcbe8eebaee
Sergey Pchelincev <jalkoby91@gmail.com>
2011-11-19 19:12:56 +0200

    bump 0.0.6

--- test_app/spec/requests/bootstrap_form_input_spec.rb
+++ test_app/spec/requests/bootstrap_form_input_spec.rb
@@ -12,14 +12,13 @@ describe SamplesController do
     page.should have_selector('.sample_form .actions a', :count=>2)
   end
 
-  it 'should have several kind of flash', :focus=>true do
+  it 'should have several kind of flash' do
     visit bootstrap_flash_messages_path
     page.should have_selector("div.customer_bootstrap_class")
     page.should have_selector("div.alert-message", :count=>4)
     page.should have_selector("div.alert-message.error", :text=>'Example of error')
     page.should have_selector("div.alert-message.success", :text=>'Example of notice')
     page.should have_selector("div.alert-message.info", :text=>'Example of info')
-    sleep 40
     first("div.alert-message.warning").text.should include('Example of warning(default flash message)')
     find("div.info a").click
     page.should_not have_selector("div.alert-message.info")
